a3bb7449e886cee16255cf66108cf412a8124608
廖子睿 <72390079+Silent-Rain02@users.noreply.github.com>
2026-08-08 18:25:28 +0800

    校验对话区场景提示

--- test_frontend_contract.py
+++ test_frontend_contract.py
@@ -38,6 +38,7 @@ class FrontendContractTests(unittest.TestCase):
             "projectSelect",
             "messageInput",
             "providerSelect",
+            "conversationContext",
             "contextDialog",
             "advanceBeat",
             "beatOutcomeInput",
